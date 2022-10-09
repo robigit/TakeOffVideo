1be37ff584f9e15610bbb1b14f0a88c1fec6838d
Roberto Grava <robertograva@hotmail.com>
2022-10-09 16:06:54 +0200

    wip

--- TakeOffVideo/wwwroot/Scripts/Recorder.js
+++ TakeOffVideo/wwwroot/Scripts/Recorder.js
@@ -34,3 +34,29 @@ export function ListaDevices(dotnetHelper, funaggiorna, funend) {
     }
 
 }
+
+
+
+export function startVideo(src, id) {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+
+        //console.info("id " + id);
+
+        navigator.mediaDevices.getUserMedia({ video: { deviceId: id } }).then(function (stream) {
+            let video = document.getElementById(src);
+            if ("srcObject" in video) {
+                video.srcObject = stream;
+            } else {
+                video.src = window.URL.createObjectURL(stream);
+            }
+
+
+            video.onloadedmetadata = function (e) {
+                video.play();
+            };
+            //mirror image
+            video.style.webkitTransform = "scaleX(-1)";
+            video.style.transform = "scaleX(-1)";
+        });
+    }
+}
