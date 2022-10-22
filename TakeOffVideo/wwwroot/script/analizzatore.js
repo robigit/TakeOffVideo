4c8e59bb367fd6bc9edb2975d829cf90d049b351
Roberto Grava <robertograva@hotmail.com>
2022-10-22 12:46:24 +0200

    Messa linea

--- TakeOffVideo/wwwroot/script/analizzatore.js
+++ TakeOffVideo/wwwroot/script/analizzatore.js
@@ -11,3 +11,40 @@ export function caricavideo(idvideo, href) {
 
     video.src = href;
 }
+
+
+export function move(direction, idcontainer, idvLine, movement) {
+
+    
+    let container = document.getElementById(idcontainer);
+    let vLine = document.getElementById(idvLine);
+
+    
+    if (container == null || vLine == null) {
+        console.error("move not found");
+    }
+    
+
+    let lineWidth = parseInt(getComputedStyle(vLine)['width'])
+    let marginLeftVline = parseInt(getComputedStyle(vLine)['margin-left']);
+    let containerWidth = parseInt(getComputedStyle(container)['width']);
+
+    //console.info(lineWidth);
+    //console.info(marginLeftVline);
+    //console.info(containerWidth);
+
+
+    switch (direction) {
+        case 'sx':
+            if (Math.abs(marginLeftVline) <= containerWidth / 2 - lineWidth - movement || marginLeftVline > 0) {
+                vLine.style['margin-left'] = marginLeftVline - movement + "px";
+               
+            }
+            break;
+        case 'dx':
+            if (Math.abs(marginLeftVline) <= containerWidth / 2 - movement || marginLeftVline < 0) {
+                vLine.style['margin-left'] = marginLeftVline + movement + "px";
+            }
+            break;
+    }
+}
