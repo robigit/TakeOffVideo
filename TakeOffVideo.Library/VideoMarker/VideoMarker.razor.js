06e5b049a8617bd540969c4de559a7ca74e439e0
Roberto Grava <robertograva@hotmail.com>
2022-10-24 22:03:17 +0200

    Incmpleto

---
+++ TakeOffVideo.Library/VideoMarker/VideoMarker.razor.js
@@ -0,0 +1,64 @@
+﻿
+export function caricavideo(video, href) {
+
+    video.src = href;
+}
+
+
+export function move(direction, container, vLine, movement) {
+
+
+
+    if (container == null || vLine == null) {
+        console.error("move not found");
+    }
+
+
+    let lineWidth = parseInt(getComputedStyle(vLine)['width'])
+    let marginLeftVline = parseInt(getComputedStyle(vLine)['margin-left']);
+    let containerWidth = parseInt(getComputedStyle(container)['width']);
+
+    //console.info(lineWidth);
+    //console.info(marginLeftVline);
+    //console.info(containerWidth);
+
+
+    switch (direction) {
+        case 'sx':
+            if (Math.abs(marginLeftVline) <= containerWidth / 2 - lineWidth - movement || marginLeftVline > 0) {
+                vLine.style['margin-left'] = marginLeftVline - movement + "px";
+
+            }
+            break;
+        case 'dx':
+            if (Math.abs(marginLeftVline) <= containerWidth / 2 - movement || marginLeftVline < 0) {
+                vLine.style['margin-left'] = marginLeftVline + movement + "px";
+            }
+            break;
+    }
+}
+
+
+export function startVideo(video, id) {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+
+        //console.info("id " + id);
+
+        navigator.mediaDevices.getUserMedia({ video: { deviceId: id } }).then(function (stream) {
+            
+            if ("srcObject" in video) {
+                video.srcObject = stream;
+            } else {
+                video.src = window.URL.createObjectURL(stream);
+            }
+
+
+            video.onloadedmetadata = function (e) {
+                video.play();
+            };
+            //mirror image
+            video.style.webkitTransform = "scaleX(-1)";
+            video.style.transform = "scaleX(-1)";
+        });
+    }
+}
