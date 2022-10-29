c9f7f492da7d0118b172030ea873e374be3f94ee
Roberto Grava <robertograva@hotmail.com>
2022-10-29 15:07:26 +0200

    VideoFileManagerTable

--- TakeOffVideo.Library/VideoFileManager/VideoFileManager.cs
+++ TakeOffVideo.Library/VideoFileManager/VideoFileManager.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Text.RegularExpressions;
 
-namespace TakeOffVideo.Services
+namespace TakeOffVideo.Library.VideoFileManager
 {
 
 
--- TakeOffVideo/Program.cs
+++ TakeOffVideo/Program.cs
@@ -1,7 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TakeOffVideo;
-using TakeOffVideo.Services;
+using TakeOffVideo.Library.VideoFileManager;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
